7804ee1ff565c8a44f3903b613c5b8a9412a6800
espitiaandres <espitiaandres123@gmail.com>
2020-08-02 15:02:42 -0400

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+import { Provider } from 'react-redux';
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -14,8 +15,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
